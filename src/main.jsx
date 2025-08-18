af859aae220ebd3ed26c71b298f90cdf862f3613
Luan <luan@cnnct.black>
2025-08-18 18:29:54 -0300

    configurar rotas do admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
+// Importar os componentes principais
+import Login from './components/ui/Login.jsx'
+import AdminPanel from './components/ui/AdminPanel.jsx'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        {/* Site público */}
+        <Route path="/" element={<App />} />
+
+        {/* Rotas do admin */}
+        <Route path="/admin" element={<Login />} />
+        <Route path="/admin/panel" element={<AdminPanel />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 )
