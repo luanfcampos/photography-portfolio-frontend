03485bcb067217678388982ce6913e0f7613dca9
Luan <luan@cnnct.black>
2025-08-18 23:46:13 -0300

    Resolvendo conflito de roteamenteo

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import Login from './components/Login.jsx'
-import AdminPanel from './components/AdminPanel.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        {/* Site público */}
-        <Route path="/" element={<App />} />
-
-        {/* Rotas do admin */}
-        <Route path="/admin/login" element={<Login />} />
-        <Route path="/admin/*" element={<AdminPanel />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </StrictMode>
 )
