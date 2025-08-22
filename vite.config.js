c1587f001d77e848b213e498a37cff5d2141c2f2
Luan <luan@cnnct.black>
2025-08-21 23:16:42 -0300

    Fix: adiciona plugin do TailwindCSS

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'   // 👈 importa o plugin
 import path from 'node:path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],          // 👈 adiciona o plugin
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
